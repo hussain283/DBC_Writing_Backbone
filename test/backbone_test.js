d6a59d13d748426d622d8e5c5af910f05eb50a62
Jared Grippe <jared@deadlyicon.com>
2013-08-12 16:12:40 -0700

    update

--- test/backbone_test.js
+++ test/backbone_test.js
@@ -1,4 +1,21 @@
 describe("Backbone", function(){
 
+  it("should be an Object", function(){
+
+    expect( typeof Backbone ).to.equal("object");
+
+
+  });
+
+});
+
+
+describe("Backbone.Model", function(){
+
+  it("should be a Function", function(){
+
+    expect(typeof Backbone.Model).to.equal('Function');
+
+  });
 
 });
